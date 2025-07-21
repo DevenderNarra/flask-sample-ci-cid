14c88553e62e7bdaa555241c91bbe215614c31a4
DevenderNarra <devendernarra@gmail.com>
2025-07-21 15:58:38 +0530

    changes in / route

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 def hello():
     return jsonify({"message": "Hello Devops"})
 
-@app.route("/about")
-def hello():
-    return jsonify({"message": "Hello about page"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
