1b84a6b7d81b82910689359e2618dc29bdc93868
DevenderNarra <devendernarra@gmail.com>
2025-08-14 14:45:26 +0530

    changed

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ def test_hello():
     client = app.app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert response.get_json() == {"message": "Hello Dev"}
+    assert response.get_json() == {"message": "Hello Devops"}
