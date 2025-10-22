3e41dd596d03d64a39199b76cd847320cd21bd75
DevenderNarra <devendernarra@gmail.com>
2025-10-22 11:18:42 +0530

    chanhes in app.py

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello():
     return jsonify({"message": "Hello from Flask!"})
 
+@app.route("/demo")
+def hello():
+    return jsonify({"message": "Demo!"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
