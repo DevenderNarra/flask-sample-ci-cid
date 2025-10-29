69161c6373b8f930b812a4800ebad7a215140a24
DevenderNarra <devendernarra@gmail.com>
2025-10-29 11:30:37 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify({"message": "Hello Devops"})
+    return jsonify({"message": "Hello"})
 
 @app.route("/health")
 def health():
